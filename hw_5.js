683971e97fd9fe37db4964e16846484dac74d12d
NurRick <nmospanov@gmail.com>
2023-01-20 15:17:42 +0600

    1-st Task

---
+++ hw_5.js
@@ -0,0 +1,26 @@
+
+// Как найти?…  1. Таблицу с `id="age-table"`
+
+const ageTable = document.getElementById('age-table');
+console.log(ageTable);
+
+// Как найти?…Все элементы label внутри этой таблицы (их три).
+
+ageTable.getElementsByTagName('label');
+
+// Первый td в этой таблице (со словом «Age»)
+
+ageTable.getElementsByTagName('td')[0];
+
+// Форму form с именем name="search".
+
+const form = document.getElementsByName('search')[0];
+
+// Первый input в этой форме.
+
+form.querySelector('input');
+
+// Последний input в этой форме.
+
+const allInputs = form.querySelectorAll('input')
+allInputs[allInputs.length-1]
